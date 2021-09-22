1cac91fd775d595b445a917a9b1d854d3a34b695
rodsam9 <sr_odriguez9@yahoo.com>
2021-09-21 20:32:34 -0500

    fixed innerHTML bug by adding defer to script tag

--- js/dates.js
+++ js/dates.js
@@ -1,5 +1,5 @@
 
 document.getElementById("date").innerHTML = new Date().getFullYear();
 
-let string = document.lastModified
-document.getElementById("last-update").innerHTML = string
+let string = document.lastModified;
+document.getElementById("last-update").innerHTML = string;
