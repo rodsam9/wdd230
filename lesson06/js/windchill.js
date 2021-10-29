bcd26b36e1b4c0b1420f00359296d5c6702fd5b2
rodsam9 <sr_odriguez9@yahoo.com>
2021-10-28 19:55:48 -0500

    changed windchill.js and changed size of hammock.jpeg

--- lesson06/js/windchill.js
+++ lesson06/js/windchill.js
@@ -2,7 +2,7 @@ let temperature = document.getElementById('temperature').textContent;
 
 let speedWind = document.getElementById('wind-speed').textContent;
 
-if(temperature <= 50 && speedWind >= 3){
+if(temperature <= 50 && speedWind > 3){
   let wind = Math.pow(speedWind, 0.16);
 
   let f = 35.74 + 0.6215 * temperature - 35.75 * wind + 0.4275 * temperature * wind;
