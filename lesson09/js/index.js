156fc8a170bee07dc8fa04b59fa487c568722871
rodsam9 <sr_odriguez9@yahoo.com>
2021-11-12 17:32:24 -0600

    updated some contact info css and some css for the towns

---
+++ lesson09/js/index.js
@@ -0,0 +1,57 @@
+const requestURL = "https://byui-cit230.github.io/weather/data/towndata.json"
+
+fetch(requestURL)
+    .then(function(response) {
+
+        return response.json();
+
+    })
+    .then(function(jsonObject) {
+
+        console.table(jsonObject);
+
+        const towns = jsonObject['towns'];
+
+        const cityNames = towns.filter(towns => 
+            (towns.name == 'Fish Haven' || 
+            towns.name == 'Preston' || 
+            towns.name == 'Soda Springs'));
+
+        cityNames.forEach(towns => {
+
+            let section = document.createElement('section');
+            let text = document.createElement("div");
+
+            let name = document.createElement('h2');
+            let motto = document.createElement('h3');
+
+            let year = document.createElement('p');
+            let population = document.createElement('p');
+            let annualRain = document.createElement('p');
+
+            let image = document.createElement('img');
+            let alt = document.createElement('alt');
+
+            name.textContent = towns.name;
+            motto.textContent = towns.motto;
+
+            year.textContent = 'Year Founded: ' + towns.yearFounded;
+            population.textContent = 'Population: ' + towns.currentPopulation;
+            annualRain.textContent = 'Annual Rainfall: ' + towns.averageRainfall;
+
+            image.setAttribute("src", `images/${towns.photo}`)
+            image.setAttribute("alt", `${towns.name} photo`);
+
+            section.appendChild(name);
+            section.appendChild(motto);
+            section.appendChild(year);
+
+            section.appendChild(population);
+            section.appendChild(annualRain);
+            section.appendChild(text);
+            section.appendChild(image);
+            section.appendChild(alt);
+
+            document.querySelector('div.towns-div').appendChild(section);
+        });
+    });
---
+++ lesson09/js/scripts.js
@@ -0,0 +1,28 @@
+const menubutton = document.querySelector('.menu');
+const mainnav = document.querySelector('.navigation')
+
+menubutton.addEventListener('click', () => 
+{mainnav.classList.toggle('responsive')}, false);
+
+// To solve the mid resizing issue with responsive class on
+
+window.onresize = () => {if (window.innerWidth > 760)
+    mainnav.classList.remove('responsive')};
+
+const d = new Date()
+const year = d.getFullYear()
+const date = d.getDate()
+const month = ['January','February','March','April','May','June','July','August','September','October','November','December']
+const monthName = month[d.getMonth()]
+const day = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday']
+const dayName = day[d.getDay()]
+const fullDate = `${dayName}, ${monthName} ${date}, ${year}`
+
+document.getElementById('time').textContent = fullDate;
+
+if (d.getDay() == 5){
+    document.getElementById('pop-up-message').style.display = "block";
+}
+else{
+    document.getElementById('pop-up-message').style.display = "none";
+}
