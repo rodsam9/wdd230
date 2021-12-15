6941c5dcff1fd3e97a6a9bd0c231adb233e8e1a8
rodsam9 <sr_odriguez9@yahoo.com>
2021-12-14 19:16:39 -0600

    updated most of index.html and imported images as well

---
+++ final/js/navigation.js
@@ -0,0 +1,9 @@
+const menubutton = document.querySelector('.menu');
+const mainnav = document.querySelector('.navigation')
+
+menubutton.addEventListener('click', () => 
+{mainnav.classList.toggle('responsive')}, false);
+
+
+window.onresize = () => {if (window.innerWidth > 760)
+    mainnav.classList.remove('responsive')};
